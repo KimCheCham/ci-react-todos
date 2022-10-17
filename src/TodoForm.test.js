025de9751dbaaaf26de460ce0435e4606fb89436
KimCheCham <errorneun@gmail.com>
2022-10-17 14:42:31 +0900

    todoForm complit

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import TodoForm from './TodoForm';
 
-describe('<TodoForm/>'), () => {
-    it('ha input and button'), () =>{
-        const {getByText, getByPlaceholderText } =render(<TodoForm/>);
+describe('<TodoForm/>', () => {
+    it('ha input and button', () =>{
+        const {getByText, getByPlaceholderText } = render(<TodoForm/>);
         getByPlaceholderText('할 일을 입력하세요');//인풋이 있는지 확인
         getByText('등록'); //버튼이 있는지 확인
-    }
-};
+    });
+});
